1209f849dbc490fad7dcd783715daa0831e9cebe
Dave Vogler <dave@sparkhouse.com>
2013-10-13 10:13:47 -0400

    new fields and models for creators and sponsors

--- articles/admin.py
+++ articles/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django import forms
-from articles.models import Article
+from articles.models import Article, Author, Sponsor, Photog, Image
+
+class ImageInline(admin.StackedInline):
+    model = Image
+    extra = 1
 
 class ArticleAdminForm(forms.ModelForm):
 	
@@ -10,8 +14,51 @@ class ArticleAdminForm(forms.ModelForm):
 class ArticleAdmin(admin.ModelAdmin): 
     prepopulated_fields = { 'slug': ['title'] }
     list_display = ('title','author','pub_date','status',)
+    inlines = [ ImageInline, ]
     form = ArticleAdminForm
-	
-	
-	
+
 admin.site.register(Article, ArticleAdmin)
+
+
+
+
+
+
+
+
+
+
+
+
+class AuthorAdminForm(forms.ModelForm):
+	
+    class Meta:
+        model = Author
+	    
+class AuthorAdmin(admin.ModelAdmin): 
+    list_display = ('name',)
+    form = AuthorAdminForm	
+
+admin.site.register(Author, AuthorAdmin)
+
+class PhotogAdminForm(forms.ModelForm):
+	
+    class Meta:
+        model = Photog
+	    
+class PhotogAdmin(admin.ModelAdmin): 
+    list_display = ('name',)
+    form = PhotogAdminForm	
+
+admin.site.register(Photog, PhotogAdmin)
+
+class SponsorAdminForm(forms.ModelForm):
+	
+    class Meta:
+        model = Sponsor
+	    
+class SponsorAdmin(admin.ModelAdmin): 
+    list_display = ('name',)
+    form = SponsorAdminForm
+    		
+admin.site.register(Sponsor, SponsorAdmin)
--- articles/models.py
+++ articles/models.py
@@ -29,12 +29,18 @@ class Article(models.Model):
     tagline = models.TextField(blank=True)
     intro = models.TextField(blank=True)
     body = models.TextField()
+
     slug = models.SlugField(unique_for_date='pub_date')
     hero = FileBrowseField("Hero Image", max_length=200, extensions=[".jpg",".png", ".gif"], blank=True, null=True, help_text='Choose an huge image for this article. Minimum 1280px wide')
     screen = models.BooleanField(default=False)
     pub_date = models.DateTimeField(default=datetime.datetime.now())
-    author = models.ForeignKey(User)
+    author = models.ForeignKey('Author', blank=True, null=True)
+    photog = models.ForeignKey('Photog', blank=True, null=True)
+    sponsor = models.ForeignKey('Sponsor', blank=True, null=True)
+    more_info = models.TextField(blank=True)
     featured = models.BooleanField(default=False)
+    special_js = models.TextField(blank=True)
+    special_css = models.TextField(blank=True, help_text='Mainly h2.unique and p.tagline')
     status = models.IntegerField(choices=STATUS_CHOICES, default=DRAFT_STATUS)
     objects = models.Manager()	
     live = LiveEntryManager()
@@ -60,5 +66,53 @@ class Article(models.Model):
     def get_absolute_url(self):
         return ('article_detail', (), {'slug': self.slug })
 	
+
+class Image(models.Model):
+    article = models.ForeignKey('Article')
+    name = models.CharField(max_length=35)
+    image = FileBrowseField("Inline IMage", max_length=200, extensions=[".jpg",".png", ".gif"], blank=True, null=True, help_text='Inline image, yo.')
+
+
+class Sponsor(models.Model):
+    name = models.CharField(max_length=250)
+    adimg = FileBrowseField("Ad Image", max_length=200, extensions=[".jpg",".png", ".gif"], blank=True, null=True, help_text='Upload an image to be used for advertising min width 250px')
+    adlink = models.URLField()
+    endorsement = models.TextField(blank=True) 
+    instances = models.IntegerField(blank=True, null=True)
+    paid_for = models.IntegerField(blank=True, null=True)
+    add_date = models.DateTimeField(default=datetime.datetime.now())    
+    
+    class Meta:
+        ordering = ['-add_date']
+        verbose_name_plural = "Sponsors"
+        #app_label = "blog"
+    	
+    def __unicode__(self):
+        return self.name  
         
+class Author(models.Model):
+    name = models.CharField(max_length=250)
+    link = models.URLField(blank=True)
+    colophon = models.TextField(blank=True) 
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = "Authors"
+        #app_label = "blog"
+    	
+    def __unicode__(self):
+        return self.name
+        
+class Photog(models.Model):
+    name = models.CharField(max_length=250)
+    link = models.URLField(blank=True)
+    colophon = models.TextField(blank=True) 
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = "Photographers"
+        #app_label = "blog"
+    	
+    def __unicode__(self):
+        return self.name         
 
