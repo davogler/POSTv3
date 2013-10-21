6bcc52858a47aa3bb1f76f554b5422655cf003a5
Dave Vogler <dave@sparkhouse.com>
2013-10-21 12:23:55 -0400

    add 404 response code to 404 handler; swap flatpage middleware fallback

--- post/settings.py
+++ post/settings.py
@@ -76,12 +76,13 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
--- post/views.py
+++ post/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render_to_response, redirect, render
 from django.template import RequestContext
 
 def custom404(request):
-    return render_to_response('404.html', context_instance=RequestContext(request))
+    
+    response = render_to_response('404.html', locals(), context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
